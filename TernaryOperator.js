28f359a3b3afd2b2eccada1e442e2944b64bb914
Purvish-Dhameliya <pbdhameliya9034@gmail.com>
2023-10-02 14:51:01 +0530

    Create TernaryOperator.js

---
+++ TernaryOperator.js
@@ -0,0 +1,8 @@
+// program to check pass or fail
+
+let marks = prompt('Enter your marks :');
+
+// check the condition
+let result = (marks >= 40) ? 'pass' : 'fail';
+
+console.log(`You ${result} the exam.`);
