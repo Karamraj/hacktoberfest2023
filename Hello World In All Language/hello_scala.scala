509d65c0ad9f93de0d04b10a889e11ddb942c29a
syedowaisalishah <113125582+syedowaisalishah@users.noreply.github.com>
2023-10-06 15:19:40 +0500

    Create HelloWorld_scala.scala

---
+++ Hello World In All Language/hello_scala.scala
@@ -0,0 +1,9 @@
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, World!")
+  }
+}
+
+//compile with in terminal using "scalac HelloWorld.scala"
+
+//run the compile program using "scala HelloWorld"
